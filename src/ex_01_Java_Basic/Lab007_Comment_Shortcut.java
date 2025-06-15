cf151773d7c24e9f95bb2052f664935b1f901866
Akash-4424 <akashpa4424@gmail.com>
2025-06-15 20:30:39 +0530

    Push

---
+++ src/ex_01_Java_Basic/Lab007_Comment_Shortcut.java
@@ -0,0 +1,9 @@
+package ex_01_Java_Basic;
+
+public class Lab007_Comment_Shortcut {
+    //This code will print the Hi
+    // Jr - QA this will print the Hii in console
+    public static void main(String[] args) {
+        System.out.println("Hii");
+    }
+}
