d74e3033b55374c2f639a069051260fd523d7c92
Akash-4424 <akashpa4424@gmail.com>
2025-06-15 20:35:05 +0530

    Push

---
+++ src/ex_01_Java_Basic/Lab008_Comments_Usage.java
@@ -0,0 +1,13 @@
+package ex_01_Java_Basic;
+
+public class Lab008_Comments_Usage {
+    /*
+    Author : Akash
+    Course : Java ATB13X
+    This is a documentation comment
+    @Akash args command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Real usgae of comments");
+    }
+}
