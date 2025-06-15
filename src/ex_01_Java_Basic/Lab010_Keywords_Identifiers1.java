86d37cd400029cae8e2d2cdbf0c739d689853a50
Akash-4424 <akashpa4424@gmail.com>
2025-06-15 20:52:25 +0530

    Push

---
+++ src/ex_01_Java_Basic/Lab010_Keywords_Identifiers1.java
@@ -0,0 +1,8 @@
+package ex_01_Java_Basic;
+
+public class Lab010_Keywords_Identifiers1 {
+    public static void main(String[] args) {
+        System.out.println("How many keywords do you see here and identifiers ");
+        System.out.println("5 3");
+    }
+}
