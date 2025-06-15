dacbf7bc545c599b1128fc6c3cc8a82819da3f49
Akash-4424 <akashpa4424@gmail.com>
2025-06-15 20:20:36 +0530

    Push

---
+++ src/ex_01_Java_Basic/Lab004_Comments.java
@@ -0,0 +1,10 @@
+package ex_01_Java_Basic;
+
+public class Lab004_Comments {
+    public static void main(String[] args) {
+        // This is a comment which will be not executed and this is single line comment
+        /*System.out.println("Hii");
+        System.out.println("Hello");
+        System.out.println("Hee");*/ // This is multi line comments
+    }
+}
