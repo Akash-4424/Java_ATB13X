f8a390bbc8d57a34b3465e90f60dc348d142ba77
Akash-4424 <akashpa4424@gmail.com>
2025-06-15 20:25:18 +0530

    Push

---
+++ src/ex_01_Java_Basic/Lab006_MultiLineComment.java
@@ -0,0 +1,11 @@
+package ex_01_Java_Basic;
+
+public class Lab006_MultiLineComment {
+    public static void main(String[] args) {
+        /*
+        Author : Akash
+        Course : Java
+         */
+        System.out.println("Multi Line Comment");
+    }
+}
